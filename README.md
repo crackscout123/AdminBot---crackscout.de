8c42b1da121f2015338286f1493a49e6d1093745
getPoland <rzepka.joel@outlook.de>
2023-03-27 02:29:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdminBot - crackscout.de
+ Its a private Adminstativ TeamSpeak3-Bot for crackscout.de
---
+++ src/de/crackscout/AdminBot/Main.java
@@ -0,0 +1,37 @@
+package de.crackscout.AdminBot;
+
+public class Main {
+
+	public static void main(String[] args) {
+		return;
+	}
+
+	
+	/**
+	 * TODO:
+	 * 
+	 * check for new teamspeak-api (java-query or rest-query?)
+	 * 
+	 * migrate default functions from crackys-ts-bot & TrollBot 
+	 * (AfkCollector.java, AntikAFK.java, de.crackscout.Logging.*, de.crackscout.utils.**)
+	 * 
+	 * improve performance particular on the subjects messaging (query->client) 
+	 * & Client collection (client->query) 
+	 * 
+	 * remove unused functions
+	 * 
+	 * clean up util's, debugging & the Log
+	 * 
+	 * 
+	 */
+	
+}
+
+
+/** 
+ *
+ * @author Joel Rzepka - crackscout.de
+ *
+ * @date 27.03.2023 - 02:19:52
+ *
+ */
